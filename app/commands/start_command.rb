6a6565c7fbb5a29ce832af9231739b81c9525e7e
vizvamitra <vizvamitra@gmail.com>
2016-05-01 20:52:06 +0300

    added commands: help, start

---
+++ app/commands/start_command.rb
@@ -0,0 +1,30 @@
+class StartCommand < BaseCommand
+  def run
+    response_text = <<-TEXT.gsub(/^ +/, '')
+      Greetings, my dear new user! ^_^
+
+      My name is Memmo and I can memorize some notes for you.
+
+      Here is the list of commands that I understand:
+
+      /add [text] - Create a new note
+      Example: /add http://example.com/blah-blah
+
+      /describe [tags] [name] - Describe last created note
+      Example: /describe #url #example Example website
+
+      /last [number] - Show given number of recently added notes
+      Example: /last 5
+
+      /tags - Show list of existing tags
+      Example: /tags
+
+      /delete [id] - Delete note with a given id
+      Example: /delete 1764
+
+      Hope you'll enjoy using me. Please write to my creator Dmitrii Krasnov at vizvamitra@gmail.com if you have any suggestions or (Oh no!) encountered a bug.
+    TEXT
+
+    respond_with(text: response_text)
+  end
+end
